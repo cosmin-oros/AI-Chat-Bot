2544e8ebd2366352276d88d72384515d7b999894
cosmin-oros <oroscosmin09@gmail.com>
2022-10-07 19:51:23 +0300

    training update

--- training.py
+++ training.py
@@ -4,12 +4,14 @@ import pickle
 import numpy as np
 import nltk
 nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
 from nltk.stem import WordNetLemmatizer
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Activation, Dropout
 from tensorflow.python.keras.optimizers import *
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open('intents.json').read())
 
@@ -26,7 +28,7 @@ for intent in intents['intents']:
         word_list = nltk.word_tokenize(pattern)
 
         # add to the words list
-        words.append(word_list)
+        words.extend(word_list)
 
         # add tupple of the word list and the respective intent tag class
         documents.append((word_list, intent['tag']))
@@ -35,4 +37,34 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-print(documents)
+words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
+# eliminate dups
+words = sorted(set(words))
+
+classes = sorted(set(classes))
+
+# save them into a file for later on
+pickle.dump(words, open('words.pk1', 'wb'))
+pickle.dump(words, open('classes.pk1', 'wb'))
+
+training = []
+output_empty = [0]*len(classes)
+
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+# shuffle the data
+random.shuffle(training)
+training = np.array(training)
+
+training_x = list(training[:, 0])
+training_y = list(training[:, 1])
